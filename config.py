be47af27b1245b3c28d577e610f00dbcedea5e6e
Lorenzo <desimone.lorenzo@protonmail.com>
2026-03-10 22:55:48 +0100

    fixed config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "velostadt-dev-secret-change-in-prod")
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "mysql+pymysql://velouser:velopass@localhost/velostadt",
+        "mysql+pymysql://velouser:velopass@mariadb/velostadt",
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET = os.environ.get("JWT_SECRET", "velostadt-jwt-secret-change-in-prod")
